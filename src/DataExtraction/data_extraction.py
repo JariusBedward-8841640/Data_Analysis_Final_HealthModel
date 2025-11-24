4b9b132738bb1b0f88e0ef5aeff6bf271561f179
Jarius School <Jbedward1640@conestogac.on.ca>
2025-11-23 20:08:46 -0500

    Loaded csv in data_extraction.py

--- src/DataExtraction/data_extraction.py
+++ src/DataExtraction/data_extraction.py
@@ -1,1 +1,17 @@
 #For if we take dataset from api or db
+import pandas as pd
+from pathlib import Path
+
+raw_path = Path("data/raw/detailed_meals_macros_.csv")
+
+
+def load_raw_data(path: Path = raw_path) -> pd.DataFrame:
+    #load raw csv into dataframe
+
+    try:
+        df = pd.read_csv(path)
+
+    except Exception:
+
+        df = pd.read_csv(path, encoding="latin1")
+    return df
